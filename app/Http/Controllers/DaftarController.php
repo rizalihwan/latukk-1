7dd1041093b6273f0761f3e1b2e6e1fed4fe026a
rizalihwan <55536560+rizalihwan@users.noreply.github.com>
2021-03-28 20:55:35 +0700

    perbaiki controller daftar

--- app/Http/Controllers/DaftarController.php
+++ app/Http/Controllers/DaftarController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Siswa;
 
 class DaftarController extends Controller
 {
     public function index()
     {
-        return view('daftar');
+        return view('daftar', [
+            'siswas' => Siswa::get()
+        ]);
     }
 }
