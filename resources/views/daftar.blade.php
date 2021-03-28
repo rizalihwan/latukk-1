3403358a2e149845b74cd6c684cd959a5bbe5c5a
rizalihwan <55536560+rizalihwan@users.noreply.github.com>
2021-03-28 19:03:48 +0700

    perbaiki tampilan daftar

--- resources/views/daftar.blade.php
+++ resources/views/daftar.blade.php
@@ -1,13 +1,37 @@
 @extends('layouts.front')
 @push('style')
     <style>
-        body
+        html, body
         {
-            background-color: red;
+            background-image: linear-gradient(#00c6ff, #0072ff);
+            background-repeat: no-repeat;
             color: white;
+            height: 100%;
         }
     </style>
 @endpush
 @section('content')
-    <h1>TETEE</h1>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="card my-5" style="width: 100%;">
+                        <div class="card-header">
+                            <h3>Silahkan registrasi</h3>
+                        </div>
+                        <div class="card-body">
+                            <div class="form-group">
+                                <input type="text" name="" id="" class="form-control">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <h1>WAW</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
