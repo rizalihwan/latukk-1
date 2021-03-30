629617b5b0789415d249480a88112eadc05c704f
rizalihwan <55536560+rizalihwan@users.noreply.github.com>
2021-03-30 08:34:24 +0700

    perbagus logout

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -60,7 +60,7 @@
             <!-- Dropdown - User Information -->
             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                 aria-labelledby="userDropdown">
-                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                <a class="dropdown-item" href="#" onclick="logout()">
                     <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                     Logout
                 </a>
@@ -70,4 +70,32 @@
     </ul>
 
 </nav>
+<form id="logout-form" action="{{ route('logout') }}" method="POST">
+    @csrf
+</form>
+<script>
+    function logout()
+    {
+        Swal.fire({
+            title: 'Message Information!',
+            text: 'Are you sure you want to exit the application?',
+            icon: 'question',
+            showCancelButton: true,
+            confirmButtonText: 'Yes, Logout!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    Swal.fire({
+                        title: "Logout...",
+                        showConfirmButton: false,
+                        timer: 2300,
+                        timerProgressBar: true,
+                        onOpen: () => {
+                            document.getElementById('logout-form').submit();
+                            Swal.showLoading();
+                        }
+                    });
+                }
+        })
+    }
+</script>
 <!-- End of Topbar -->
